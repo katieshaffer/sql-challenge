734a94f98c60a53700946b5fd67140fa62c83197
katieshaffer <104028216+katieshaffer@users.noreply.github.com>
2024-03-04 21:51:42 -0500

    Update case_study_1.sql

--- Case Study 1: Danny's Diner/case_study_1.sql
+++ Case Study 1: Danny's Diner/case_study_1.sql
@@ -77,23 +77,143 @@ WITH first_order AS
 
 --Using `DISTINCT` here because `STRING_AGG` doesn't work with `DISTINCT`
 SELECT DISTINCT sales.customer_id
-	 , STRING_AGG(product_name, ', ') OVER (PARTITION BY sales.customer_id) AS first_products_ordered
+	 , STRING_AGG(product_name, ', ') AS first_products_ordered
 FROM dannys_diner.sales
 JOIN first_order ON sales.order_date = first_order.first_order_date
 JOIN dannys_diner.menu ON sales.product_id = menu.product_id
 GROUP BY sales.customer_id, product_name;
 
-
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT menu.product_name
+     , COUNT(sales.product_id) AS number_of_orders
+FROM dannys_diner.sales
+INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+GROUP BY sales.product_id
+       , menu.product_name
+ORDER BY number_of_orders DESC
+LIMIT 1;
 
 -- 5. Which item was the most popular for each customer?
+WITH customer_product_orders AS
+(
+  SELECT sales.customer_id
+       , menu.product_name
+       , COUNT(sales.product_id) AS number_of_orders
+       , DENSE_RANK() OVER (PARTITION BY sales.customer_id 
+			    ORDER BY COUNT(sales.customer_id) DESC) AS rank 
+  FROM dannys_diner.sales
+  INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+  GROUP BY sales.customer_id
+         , menu.product_name
+ )
+ 
+ SELECT DISTINCT customer_id
+      --If there is a tie, then list all the top products
+      , STRING_AGG(product_name, ', ') AS most_ordered_products
+      , number_of_orders
+ FROM customer_product_orders
+ WHERE rank = 1
+ GROUP BY customer_id
+        , number_of_orders
+;
 
 -- 6. Which item was purchased first by the customer after they became a member?
+WITH orders_after_signup AS
+(
+  SELECT sales.customer_id
+       , members.join_date
+       , sales.order_date
+       , menu.product_name
+       , DENSE_RANK() OVER (PARTITION BY sales.customer_id 
+			    ORDER BY order_date) AS rank 
+  FROM dannys_diner.sales
+  INNER JOIN dannys_diner.members ON sales.customer_id = members.customer_id
+  INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+  WHERE members.join_date < sales.order_date --Only pull orders after join date
+  GROUP BY sales.customer_id
+  	 , members.join_date
+  	 , sales.order_date
+         , menu.product_name
+)
+
+SELECT customer_id
+     , product_name
+FROM orders_after_signup
+WHERE rank = 1;
 
 -- 7. Which item was purchased just before the customer became a member?
+WITH orders_before_signup AS
+(
+  SELECT sales.customer_id
+       , members.join_date
+       , sales.order_date
+       , menu.product_name
+       , DENSE_RANK() OVER (PARTITION BY sales.customer_id 
+			    ORDER BY order_date DESC) AS rank 
+  FROM dannys_diner.sales
+  INNER JOIN dannys_diner.members ON sales.customer_id = members.customer_id
+  INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+  WHERE members.join_date > sales.order_date --Only pull orders before join date
+  GROUP BY sales.customer_id
+  	 , members.join_date
+  	 , sales.order_date
+         , menu.product_name
+)
+
+SELECT customer_id
+     , STRING_AGG(product_name, ', ') as products_ordered_before_joining
+FROM orders_before_signup
+WHERE rank = 1
+GROUP BY customer_id;
 
 -- 8. What is the total items and amount spent for each member before they became a member?
+SELECT sales.customer_id
+     , COUNT(sales.product_id) AS number_of_orders
+     , SUM(menu.price) AS total_spent
+FROM dannys_diner.sales
+INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+INNER JOIN dannys_diner.members ON sales.customer_id = members.customer_id
+WHERE sales.order_date < members.join_date
+GROUP BY sales.customer_id;
 
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+WITH order_type AS
+(
+  SELECT sales.customer_id
+       , SUM(CASE WHEN menu.product_name != 'sushi' THEN price ELSE 0 END) AS num_non_sushi_orders
+       , SUM(CASE WHEN menu.product_name = 'sushi' THEN price ELSE 0 END) AS num_sushi_orders
+  FROM dannys_diner.sales
+  INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+  GROUP BY sales.customer_id
+         , menu.product_name
+)
+
+SELECT customer_id
+     , (SUM(num_non_sushi_orders) * 10) + (SUM(num_sushi_orders) * 20) AS points_earned
+FROM order_type
+GROUP BY customer_id;
 
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January? 
+WITH double_rewards_week AS
+(
+  SELECT members.customer_id
+  , join_date
+  , join_date + INTERVAL '6 day' AS end_of_double_rewards
+  , order_date
+  , menu.product_name
+  , menu.price
+  FROM dannys_diner.members
+  INNER JOIN dannys_diner.sales ON members.customer_id = sales.customer_id
+  INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
+  WHERE sales.order_date <= '2021-01-31'
+  AND order_date >= join_date
+)
+
+
+SELECT customer_id
+     , SUM(CASE WHEN product_name = 'sushi' THEN price * 20
+                WHEN order_date BETWEEN join_date AND end_of_double_rewards
+                THEN price * 20
+           ELSE price * 10 END) AS points_earned
+FROM double_rewards_week
+GROUP BY customer_id
