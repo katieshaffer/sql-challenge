180512eab5d2a7552256b9840e0287d17035b290
Katie Shaffer <104028216+katieshaffer@users.noreply.github.com>
2025-03-09 17:09:51 -0400

    Update case_study_2.sql

--- Case Study 2: Pizza Runner/case_study_2.sql
+++ Case Study 2: Pizza Runner/case_study_2.sql
@@ -19,7 +19,7 @@ WHERE COALESCE(cancellation, '') IN ('', 'null');
 SELECT customer_orders.pizza_id
      , COUNT(customer_orders.*) AS number_of_pizzas_delivered
 FROM pizza_runner.runner_orders
-JOIN pizza_runner.customer_orders ON runner_orders.order_id = customer_orders.order_id
+INNER JOIN pizza_runner.customer_orders ON runner_orders.order_id = customer_orders.order_id
 WHERE COALESCE(cancellation, '') IN ('', 'null')
 GROUP BY customer_orders.pizza_id;
 
@@ -27,14 +27,14 @@ GROUP BY customer_orders.pizza_id;
 SELECT pizza_name
      , COUNT(customer_orders.*) AS number_of_pizzas_ordered
 FROM pizza_runner.customer_orders
-JOIN pizza_runner.pizza_names ON customer_orders.pizza_id = pizza_names.pizza_id
+INNER JOIN pizza_runner.pizza_names ON customer_orders.pizza_id = pizza_names.pizza_id
 GROUP BY pizza_name;
 
 --What was the maximum number of pizzas delivered in a single order?
 SELECT runner_orders.order_id
      , COUNT(customer_orders.*) AS number_of_pizzas_delivered
 FROM pizza_runner.runner_orders
-JOIN pizza_runner.customer_orders ON runner_orders.order_id = customer_orders.order_id
+INNER JOIN pizza_runner.customer_orders ON runner_orders.order_id = customer_orders.order_id
 WHERE COALESCE(cancellation, '') IN ('', 'null')
 GROUP BY runner_orders.order_id
 ORDER BY number_of_pizzas_delivered DESC
